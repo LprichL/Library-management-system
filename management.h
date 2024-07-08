4cd1fd93b91857d810ccbbdd5a4aa8cfba1c85f8
liupan <lp2548836497@163.com>
2024-07-08 22:54:14 +0800

    纠正。management.h文件

--- management.h
+++ management.h
@@ -21,23 +21,26 @@ struct Node
 };
 
 // 创建头节点
-struct Node* CreateHead(struct book data);
+struct Node* CreateHead();
+
+// 创建新节点
+struct Node* CreateNewCode(struct book data);
 
 // 插入书籍
 void InsertBook(struct Node* HeadNode, struct book data);
 
 // 删除书籍
-void DeletBookbyName(struct Node* HeadNode, char name); // 通过书名删除
+void DeletBook_by_Name(struct Node* HeadNode, char name); // 通过书名删除
 
-void DeletBookbyName(struct Node* HeadNode, int id); // 通过编号删除
+void DeletBook_by_id(struct Node* HeadNode, int id); // 通过编号删除
 
 // 修改书籍信息
 void ResetBook(struct Node* HeadNode, struct book data);
 
 // 查找书籍
-void FindBook(struct Node* HeadNode, int id); // 通过编号查找
+void FindBook_by_id(struct Node* HeadNode, int id); // 通过编号查找
 
-void FindBook(struct Node* HeadNode, char name); // 通过书名查找
+void FindBook_by_name(struct Node* HeadNode, char name); // 通过书名查找
 
 // 打印出具体信息
 void PrintBook(struct Node* HeadNode);
